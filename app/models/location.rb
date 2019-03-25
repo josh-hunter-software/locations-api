4c8235f89fcc2bc12dbdd20059e7556e4448972e
Josh Hunter <thatsmegettinmail@yahoo.com>
2019-03-25 18:26:13 -0500

    add distance scope

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+  scope :within, -> (longitude, latitude, distance_in_mile = 1) {
+  where(%{
+   ST_Distance(lonlat, 'POINT(%f %f)') < %d
+  } % [longitude, latitude, distance_in_mile * 1609.34])
+}
 end
